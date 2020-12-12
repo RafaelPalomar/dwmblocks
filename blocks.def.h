cb045626b1c7982776be11d52fb322762ddd8074
Rafael Palomar <rafaelpalomaravalos@gmail.com>
2020-12-12 13:45:26 +0100

    Adding checkmail

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "wifi.sh",                                                     30,     0},
-	{"", "audio.sh",                                                    30,     1},
-	{"", "battery.sh",                                                  30,     0},
-	{" ", "memory.sh",	                                            30,		0},
-	{" ", "df -h |grep /$|tr -s ' '| cut -d' ' -f 4",	                30,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					                 5,		0},
-	{"", "",                       					                 5,		0},
+    {"", "checkmail.sh",                                                        600,     2},
+	{"", "wifi.sh",                                                              30,     0},
+	{"", "audio.sh",                                                             30,     1},
+	{"", "battery.sh",                                                           30,     0},
+	{" ", "memory.sh",	                                                     30,  	 0},
+	{" ", "df -h |grep /$|tr -s ' '| cut -d' ' -f 4",	                         30,	 0},
+	{"", "date '+%b %d (%a) %I:%M%p'",					                          5,     0},
+	{"", "",                       					                          5,	 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
